bb6763edd5785f96efee86935d36c5b80b06f21a
Clément Malige <clement@rakam.ai>
2024-10-23 17:06:20 +0200

    unit test

---
+++ UDPClientTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UDPClientTest {
+
+    @org.junit.jupiter.api.Test
+    void launch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
